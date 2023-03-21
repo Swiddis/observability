7e29ead73a30e09d3d1ff38ccdf81fafb6d442d8
Simeon Widdis <sawiddis@amazon.com>
2023-03-21 14:01:35 -0700

    Stub integration directory creation
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- scripts/integrations-cli/integrations-cli.py
+++ scripts/integrations-cli/integrations-cli.py
@@ -1,5 +1,7 @@
 #!./venv/bin/python3
 import click
+import os
+import json
 
 
 @click.group()
@@ -8,10 +10,35 @@ def integrations_cli():
     pass
 
 
+def make_config(name: str, path: str) -> dict:
+    return {
+        "name": name,
+        "version": {"integ": "0.1.0", "schema": "1.0.0", "resource": "^1.23.0"},
+        "description": "",
+        "identification": "",
+        "catalog": "observability",
+        "components": [],
+        "collection": [],
+        "repo": {"github": "file://" + path},
+    }
+
+
 @integrations_cli.command()
-def create():
+@click.argument("name")
+def create(name: str):
     """Create a new Integration from a specified template."""
-    click.echo("Created integration")
+    click.echo(f"Creating new integration '{name}'")
+    integration_path = os.path.join(os.getcwd(), "integrations", name)
+    if os.path.exists(integration_path) and len(os.listdir(integration_path)) != 0:
+        raise click.ClickException(
+            f"destination path '{integration_path}' exists and is non-empty"
+        )
+    os.makedirs(integration_path, exist_ok=True)
+    cfg_path = os.path.join(integration_path, "config.json")
+    with open(cfg_path, "w") as config:
+        json.dump(
+            make_config(name, cfg_path), config, ensure_ascii=False, indent=2
+        )
 
 
 @integrations_cli.command()
