4ffc49a6396f43cb2ef3a4804cde991ac0a364b8
Simeon Widdis <sawiddis@amazon.com>
2023-03-21 15:33:05 -0700

    Check for schema existence in validation
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- scripts/integrations-cli/helpers/validate.py
+++ scripts/integrations-cli/helpers/validate.py
@@ -1,7 +1,13 @@
+import logging
+
 import jsonschema
+
+from .constants import SCHEMAS
 
 
 def validate_config(config: dict) -> dict:
-    assert isinstance(config["template-name"], str)
-    assert isinstance(config["repository"]["url"], str)
+    if "integration.schema" in SCHEMAS:
+        jsonschema.validate(SCHEMAS["integration.schema"], config)
+    else:
+        logging.warn("Config schema not found, skipping config validation")
     return config
